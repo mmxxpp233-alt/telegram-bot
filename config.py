f2a0ac4e1418bbeed3420394d545914bd132fde9
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:23:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN is missing! Add it in Railway Variables")
+
 # 👇 قنوات الاشتراك الإجباري
 CHANNELS = [
     "@feraon_1",
@@ -13,7 +16,7 @@ CHANNELS = [
 HEADER_IMAGE = "https://i.postimg.cc/sXgH7H06/IMG-20260620-014329.png"
 MENU_IMAGE = "https://i.postimg.cc/ydKs1Kx3/IMG-20260611-165810.jpg"
 
-# 👇 بياناتك (حقوق المطور)
+# 👇 بياناتك
 OWNER_NAME = "محمد السيد 🟦"
 OWNER_USERNAME = "@ATTACK_V12"
 OWNER_ID = 7771042305
