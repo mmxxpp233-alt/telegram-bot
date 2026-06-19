040bc3b41d2613070c02e91d6bf85fd4dd06c669
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 01:46:36 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# 👇 قنوات الاشتراك الإجباري
+CHANNELS = [
+    "@feraon_1",
+    "@my_botg1",
+    "@fraon10k"
+]
+
+# 👇 الصور
+HEADER_IMAGE = "https://i.postimg.cc/sXgH7H06/IMG-20260620-014329.png"
+MENU_IMAGE = "https://i.postimg.cc/ydKs1Kx3/IMG-20260611-165810.jpg"
+
+# 👇 بياناتك (حقوق المطور)
+OWNER_NAME = "محمد السيد 🟦"
+OWNER_USERNAME = "@ATTACK_V12"
+OWNER_ID = 7771042305
+
+# 👇 اسم البوت
+BOT_NAME = "𝐀𝐋𝐌𝐍𝐇𝐑𝐅↝ ❶"
+
+# 👇 الرسائل
+JOIN_MESSAGE = "يجب الاشتراك في قنوات المطور أولاً لكي تتمكن من استخدام البوت 🛡️"
+
+SUCCESS_MESSAGE = "شكراً لاشتراكك في البوت 💓✨"
