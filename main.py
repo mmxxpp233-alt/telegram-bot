ae2339ad1e21f4f593c236d9552c8d8a92d6fd01
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:51:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from config import BOT_TOKEN
-
-# logging عشان لو في مشكلة تظهر في Railway Logs
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 البوت شغال 100%")
+from handlers.start import start_handler
+from handlers.check import check_handler
+from handlers.menu import menu_handler
 
 def main():
-    print("Starting bot...")
-
-    if not BOT_TOKEN:
-        print("ERROR: BOT_TOKEN is missing")
-        return
-
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start_handler))
+    app.add_handler(CallbackQueryHandler(check_handler, pattern="check"))
+    app.add_handler(CallbackQueryHandler(menu_handler))
 
     print("Bot is running...")
     app.run_polling()
