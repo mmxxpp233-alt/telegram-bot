371b9feb2d2aefbaf7d46c1570f1f7b42a32fda1
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:49:20 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,21 +1,18 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import ContextTypes
-from config import HEADER_IMAGE, JOIN_MESSAGE, CHANNELS
-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def keyboard():
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton("📢 قناة 1", url=CHANNELS[0])],
-        [InlineKeyboardButton("📢 قناة 2", url=CHANNELS[1])],
-        [InlineKeyboardButton("📢 قناة 3", url=CHANNELS[2])],
+        [InlineKeyboardButton("📢 قناة 1", url="https://t.me/feraon_1")],
+        [InlineKeyboardButton("📢 قناة 2", url="https://t.me/my_botg1")],
+        [InlineKeyboardButton("📢 قناة 3", url="https://t.me/fraon10k")],
         [InlineKeyboardButton("✅ تحقق", callback_data="check")]
     ])
 
+async def start_handler(update, context):
+    from config import HEADER_IMAGE, JOIN_MESSAGE
 
-async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message:
-        await update.message.reply_photo(
-            photo=HEADER_IMAGE,
-            caption=JOIN_MESSAGE,
-            reply_markup=keyboard()
-        )
+    await update.message.reply_photo(
+        photo=HEADER_IMAGE,
+        caption=JOIN_MESSAGE,
+        reply_markup=keyboard()
+    )
