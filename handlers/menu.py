0e74db91fb9e3a84c609e11f163da3194daa9d02
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:17:56 +0300

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,18 +1,28 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from config import MENU_IMAGE, OWNER_USERNAME
 
+
 def menu_keyboard():
     return InlineKeyboardMarkup([
         [InlineKeyboardButton("📱 الحصول على رقم", callback_data="get_number")],
         [InlineKeyboardButton("📜 الشروط", callback_data="rules")]
     ])
 
-async def show_menu(message):
-    await message.reply_photo(
+
+async def show_menu(update):
+    await update.message.reply_photo(
         photo=MENU_IMAGE,
         caption=f"🏠 اللوحة الرئيسية\n👨‍💻 المطور: {OWNER_USERNAME} 🟦",
         reply_markup=menu_keyboard()
     )
 
+
 async def menu_handler(update, context):
-    await update.callback_query.answer()
+    query = update.callback_query
+    await query.answer()
+
+    await query.message.reply_photo(
+        photo=MENU_IMAGE,
+        caption=f"🏠 اللوحة الرئيسية\n👨‍💻 المطور: {OWNER_USERNAME} 🟦",
+        reply_markup=menu_keyboard()
+    )
