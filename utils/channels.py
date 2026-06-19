526ef21e65cfc7d81cda6da700eadacc2e4e350b
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:48:23 +0300

    Update channels.py

--- utils/channels.py
+++ utils/channels.py
@@ -1,16 +1,6 @@
-from config import CHANNELS
-from telegram.error import TelegramError
-
-
-async def is_joined(user_id, bot):
-    try:
-        for ch in CHANNELS:
-            member = await bot.get_chat_member(chat_id=ch, user_id=user_id)
-
-            if member.status in ["left", "kicked"]:
-                return False
-
-        return True
-
-    except TelegramError:
-        return False
+async def is_joined(user_id, bot, channels):
+    for ch in channels:
+        member = await bot.get_chat_member(ch, user_id)
+        if member.status in ["left", "kicked"]:
+            return False
+    return True
