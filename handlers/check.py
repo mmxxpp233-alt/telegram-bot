bce1423516dfea8237992648b48185dc114c9a24
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:18:36 +0300

    Update check.py

--- handlers/check.py
+++ handlers/check.py
@@ -1,15 +1,24 @@
 from telegram.ext import ContextTypes
 from utils.channels import is_joined
-from handlers.menu import show_menu
-from config import SUCCESS_MESSAGE
+from handlers.menu import menu_keyboard
+from config import SUCCESS_MESSAGE, MENU_IMAGE, OWNER_USERNAME
+
 
 async def check_handler(update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+    await query.answer()
+
     user_id = query.from_user.id
 
     if await is_joined(user_id, context.bot):
-        await query.message.delete()
-        await query.message.chat.send_message(SUCCESS_MESSAGE)
-        await show_menu(query.message)
+
+        await query.message.reply_text(SUCCESS_MESSAGE)
+
+        await query.message.reply_photo(
+            photo=MENU_IMAGE,
+            caption=f"🏠 اللوحة الرئيسية\n👨‍💻 المطور: {OWNER_USERNAME} 🟦",
+            reply_markup=menu_keyboard()
+        )
+
     else:
         await query.answer("❌ لازم تشترك في كل القنوات", show_alert=True)
