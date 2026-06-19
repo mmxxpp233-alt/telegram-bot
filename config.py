9fd3c8736ab28d8b54b3ff6881dc9e2c6a8aa7bc
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 02:25:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,3 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("❌ BOT_TOKEN is missing! Add it in Railway Variables")
-
-# 👇 قنوات الاشتراك الإجباري
-CHANNELS = [
-    "@feraon_1",
-    "@my_botg1",
-    "@fraon10k"
-]
-
-# 👇 الصور
-HEADER_IMAGE = "https://i.postimg.cc/sXgH7H06/IMG-20260620-014329.png"
-MENU_IMAGE = "https://i.postimg.cc/ydKs1Kx3/IMG-20260611-165810.jpg"
-
-# 👇 بياناتك
-OWNER_NAME = "محمد السيد 🟦"
-OWNER_USERNAME = "@ATTACK_V12"
-OWNER_ID = 7771042305
-
-# 👇 اسم البوت
-BOT_NAME = "𝐀𝐋𝐌𝐍𝐇𝐑𝐅↝ ❶"
-
-# 👇 الرسائل
-JOIN_MESSAGE = "يجب الاشتراك في قنوات المطور أولاً لكي تتمكن من استخدام البوت 🛡️"
-
-SUCCESS_MESSAGE = "شكراً لاشتراكك في البوت 💓✨"
