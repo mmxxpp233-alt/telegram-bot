3169aadbb17188052538866a532e3c5ddb266a1e
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 01:47:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+from config import BOT_TOKEN
+from handlers.start import start_handler
+from handlers.check import check_handler
+from handlers.menu import menu_handler
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start_handler))
+    app.add_handler(CallbackQueryHandler(check_handler, pattern="check"))
+    app.add_handler(CallbackQueryHandler(menu_handler))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
