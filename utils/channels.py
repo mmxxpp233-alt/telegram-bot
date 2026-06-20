562aa8621f107e48769271167380be95aded28af
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 03:41:15 +0300

    Delete utils/channels.py

--- utils/channels.py
+++
@@ -1,6 +0,0 @@
-async def is_joined(user_id, bot, channels):
-    for ch in channels:
-        member = await bot.get_chat_member(ch, user_id)
-        if member.status in ["left", "kicked"]:
-            return False
-    return True
