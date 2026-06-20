a72349b4d335bf85a1846f41d71af98b2301d978
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 03:39:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,3 @@
-import os
+BOT_TOKEN = "PUT_TOKEN"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-CHANNELS = [
-    "@feraon_1",
-    "@my_botg1",
-    "@fraon10k"
-]
-
-HEADER_IMAGE = "https://i.postimg.cc/sXgH7H06/IMG-20260620-014329.png"
-MENU_IMAGE = "https://i.postimg.cc/ydKs1Kx3/IMG-20260611-165810.png"
-
-OWNER_NAME = "محمد السيد 🟦"
-OWNER_USERNAME = "@ATTACK_V12"
-
-JOIN_MESSAGE = "يجب الاشتراك في قنوات المطور أولاً 🛡️"
-SUCCESS_MESSAGE = "شكراً لاشتراكك في البوت 💓✨"
+BOT_NAME = "𝐀𝐋𝐌𝐍𝐇𝐑𝐅 VIP"
