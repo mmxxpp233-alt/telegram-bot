5e88f09735eb758d423ef31a8c73739791160e15
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 05:55:46 +0300

    Delete generator.py

--- generator.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def generate_code():
-    return random.randint(100000, 999999)
-
-def generate_number(country_code):
-    return f"{country_code} XXX XXX {random.randint(1000,9999)}"
