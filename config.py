f93a9d334a4ab5ad5d7f7f0e4a5007be9c527f0c
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-20 06:20:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# رابط الصورة اللي هتظهر في start
-START_IMAGE = os.getenv("https://i.postimg.cc/sXgH9H07/IMG-20260620-014329.png")
+START_IMAGE = os.getenv("START_IMAGE")
