eaab4c5a41ba5213898d2048d819d3befc4ebd0d
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-22 05:29:43 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from keyboards.menus import main_menu
 from config import START_IMAGE
+from handlers.subscription import check_subscriptions
 
 router = Router()
 
@@ -10,14 +12,28 @@ router = Router()
 @router.message(CommandStart())
 async def start_cmd(message):
 
-    if START_IMAGE:
+    ok = await check_subscriptions(message.bot, message.from_user.id)
+
+    if not ok:
+
+        kb = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text="📢 قناة 1", url="https://t.me/feraon_1")],
+                [InlineKeyboardButton(text="📢 قناة 2", url="https://t.me/my_botg1")],
+                [InlineKeyboardButton(text="📢 قناة 3", url="https://t.me/fraon10k")],
+                [InlineKeyboardButton(text="✅ تحقق", callback_data="check_sub")]
+            ]
+        )
+
         await message.answer_photo(
             photo=START_IMAGE,
-            caption="👋 أهلاً بك في البوت\n\nاختر من القائمة بالأسفل",
-            reply_markup=main_menu()
+            caption="⚠️ لازم تشترك في القنوات أولاً",
+            reply_markup=kb
         )
-    else:
-        await message.answer(
-            "👋 أهلاً بك في البوت\n\nاختر من القائمة بالأسفل",
-            reply_markup=main_menu()
-        )
+        return
+
+    await message.answer_photo(
+        photo=START_IMAGE,
+        caption="👋 أهلاً بك في البوت",
+        reply_markup=main_menu()
+    )
