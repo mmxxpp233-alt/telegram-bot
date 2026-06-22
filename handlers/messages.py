18c6e04213efb9ac7a22260645fed5b24bc00f74
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-22 07:50:07 +0300

    Update messages.py

--- handlers/messages.py
+++ handlers/messages.py
@@ -11,28 +11,22 @@ router = Router()
 @router.message(F.photo)
 async def handle_photo(message: Message, state: FSMContext):
 
-    # نتأكد إن المستخدم داخل وضع QR فقط
     if await state.get_state() != "qr_wait":
         return
 
-    # تحميل الصورة من تيليجرام
     file = await message.bot.get_file(message.photo[-1].file_id)
 
     path = f"qr_{message.from_user.id}.png"
 
     await message.bot.download_file(file.file_path, path)
 
-    # قراءة QR
     result = read_qr(path)
 
-    # حذف الصورة بعد الاستخدام
     os.remove(path)
 
-    # إنهاء الحالة
     await state.clear()
 
-    # الرد على المستخدم
     if result:
-        await message.answer(f"📄 تم قراءة QR Code:\n\n{result}")
+        await message.answer(f"📄 تم قراءة QR:\n\n{result}")
     else:
-        await message.answer("❌ الصورة لا تحتوي على QR Code")
+        await message.answer("❌ مفيش QR في الصورة")
