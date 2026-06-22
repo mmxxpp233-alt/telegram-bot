ad20aec0f7ca8619b04635b3bf88fc8c83b55d42
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-22 07:56:02 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,13 +1,18 @@
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 router = Router()
+
+
+class QRState(StatesGroup):
+    waiting = State()
 
 
 @router.callback_query(F.data == "barcode_read")
 async def barcode_read(call, state: FSMContext):
 
-    await state.set_state("qr_wait")
+    await state.set_state(QRState.waiting)
 
     await call.answer()
 
