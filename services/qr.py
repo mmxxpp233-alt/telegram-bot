2f6072ebdaac96bf4ad5ff2213da10914f59e83f
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-22 07:38:12 +0300

    Create qr.py

---
+++ services/qr.py
@@ -0,0 +1,11 @@
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def read_qr(path):
+    img = Image.open(path)
+    data = decode(img)
+
+    if not data:
+        return None
+
+    return data[0].data.decode("utf-8")
