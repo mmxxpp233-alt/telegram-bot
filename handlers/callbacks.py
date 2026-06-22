8d7582b40c40e0b76557dd5af4f3fc65c3ced31f
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-22 07:40:59 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,20 +1,24 @@
 from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
 
 router = Router()
 
 
+# 📱 رقم جديد
 @router.callback_query(F.data == "new_number")
 async def new_number(call):
     await call.answer()
     await call.message.answer("📱 ميزة رقم جديد سيتم تطويرها قريبًا")
 
 
+# 🔊 تحويل نص لصوت
 @router.callback_query(F.data == "tts")
 async def tts(call):
     await call.answer()
     await call.message.answer("🔊 أرسل النص لتحويله لصوت")
 
 
+# ✨ زخرفة
 @router.callback_query(F.data == "decorate")
 async def decorate(call):
     await call.answer()
@@ -40,3 +44,14 @@ async def info(call):
 async def settings(call):
     await call.answer()
     await call.message.answer("⚙️ الإعدادات سيتم تطويرها لاحقًا")
+
+
+# 📷 قراءة QR Code
+@router.callback_query(F.data == "barcode_read")
+async def barcode_read(call, state: FSMContext):
+
+    await state.set_state("qr_wait")
+
+    await call.answer()
+
+    await call.message.answer("📷 ابعت صورة فيها QR Code")
