be411365ea5f71e6142673e7ad70163e0bd95478
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 10:20:18 +0300

    Update bot_commands.py

--- bot_commands.py
+++ bot_commands.py
@@ -4,9 +4,7 @@ from aiogram.types import BotCommand
 
 async def set_commands(bot: Bot):
     commands = [
-        BotCommand(command="start", description="🚀 تشغيل البوت"),
-        BotCommand(command="how_to_use", description="📖 شرح استخدام البوت"),
-        BotCommand(command="chat_cleanup", description="🧹 تنظيف الشات"),
+        BotCommand(command="start", description="بدا البوت 🤖"),
     ]
 
     await bot.set_my_commands(commands)
