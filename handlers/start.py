939b75984b38e75b8efd93274db1bf370316a7d0
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 21:56:09 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -8,41 +8,47 @@ from keyboards.menus import main_menu
 
 router = Router()
 
-
 def subscribe_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❶", url="https://t.me/feraon_1")],
-        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❷", url="https://t.me/my_botg1")],
-        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❸", url="https://t.me/fraon10k")],
-        [InlineKeyboardButton(text="✅ تحقق", callback_data="check_sub")]
-    ])
-
+return InlineKeyboardMarkup(inline_keyboard=[
+[InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❶", url="https://t.me/feraon_1")],
+[InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❷", url="https://t.me/my_botg1")],
+[InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❸", url="https://t.me/fraon10k")],
+[InlineKeyboardButton(text="✅ تحقق", callback_data="check_sub")]
+])
 
 @router.message(CommandStart())
 async def start_cmd(message):
 
-    ok = await check_subscriptions(message.bot, message.from_user.id)
+ok = await check_subscriptions(message.bot, message.from_user.id)  
 
-if not ok:
-await message.answer_photo(
-photo=SUB_IMAGE,
-caption=f"""
-اهلا بك عزيزي {message.from_user.first_name}
+if not ok:  
+    await message.answer_photo(  
+        photo=SUB_IMAGE,  
+        caption=f"""
 
-يرجى الاشتراك في قنوات المطور لكي تتمكن من استخدام البوت
+👋 أهلاً بك يا {message.from_user.first_name}
+
+⚠️ لازم تشترك في القنوات أولاً
+وبعدها اضغط تحقق 👇
 """,
 reply_markup=subscribe_keyboard()
 )
 return
 
-await message.answer_photo(
-photo=START_IMAGE,
-caption=f"""
-اهلا بك {message.from_user.first_name}
+await message.answer_photo(  
+    photo=START_IMAGE,  
+    caption=f"""
 
+👋 أهلاً بك عزيزي {message.from_user.first_name}
 🤖 {BOT_NAME}
 
-اختر من القائمة 🖥️
+اختر من القائمة 👇
 """,
 reply_markup=main_menu()
 )
+
+عاوز اعدل
+👋 أهلاً بك يا  اسم الشخص
+
+⚠️ لازم تشترك في القنوات أولاً
+وبعدها اضغط تحقق 👇
