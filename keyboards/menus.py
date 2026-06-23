21d6183708237e28b44cc84fefe5bb6a7b06ddb6
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 11:08:56 +0300

    Update menus.py

--- keyboards/menus.py
+++ keyboards/menus.py
@@ -1,44 +1,28 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
 def main_menu():
     return InlineKeyboardMarkup(inline_keyboard=[
-
         [
             InlineKeyboardButton(text="📷 قراءة QR", callback_data="qr_read"),
             InlineKeyboardButton(text="🧾 إنشاء QR", callback_data="qr_create")
-       ],
-
+        ],
         [
             InlineKeyboardButton(text="🔗 فحص الروابط", callback_data="check_link"),
             InlineKeyboardButton(text="✂️ اختصار الروابط", callback_data="short_link")
         ],
-
         [
-            InlineKeyboardButton(text="🔊 تحويل لصوت", callback_data="tts"),
-            InlineKeyboardButton(text="✨ زخرفة", callback_data="decorate")
+            InlineKeyboardButton(text="✨ زخرفة أسماء", callback_data="decorate"),
+            InlineKeyboardButton(text="🔊 تحويل نص لصوت", callback_data="tts")
         ],
-
         [
             InlineKeyboardButton(text="👤 إنشاء يوزر", callback_data="new_user"),
             InlineKeyboardButton(text="🤖 إنشاء بوت", callback_data="create_bot")
         ],
-
         [
-            InlineKeyboardButton(text="👨‍💻 المطور", callback_data="developer"),
-            InlineKeyboardButton(text="🛡️ حماية IP", callback_data="ip_protect")
-        ],
-
-        [
-            InlineKeyboardButton(text="ℹ️ معلومات", callback_data="info"),
-            InlineKeyboardButton(text="⚙️ الإعدادات", callback_data="settings")
-        ],
-
-        # الأزرار الجانبية للمستخدم
-        [
-            InlineKeyboardButton(text="🧹 مسح الشات", callback_data="clear_chat")
+            InlineKeyboardButton(text="🛡️ حماية IP", callback_data="ip_protect"),
+            InlineKeyboardButton(text="ℹ️ معلوماتك كـ مستخدم", callback_data="user_info")
         ],
         [
-            InlineKeyboardButton(text="📖 شرح البوت", callback_data="bot_help")
+            InlineKeyboardButton(text="👨‍💻 المطور", callback_data="developer")
         ]
     ])
