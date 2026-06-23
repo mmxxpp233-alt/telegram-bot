9bfb4bf078e24f79f8e13ef31e49ae5d068410b9
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 06:09:30 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,51 +9,32 @@ from handlers.subscription import check_subscriptions
 router = Router()
 
 
+def subscribe_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❶", url="https://t.me/feraon_1")],
+        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❷", url="https://t.me/my_botg1")],
+        [InlineKeyboardButton(text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❸", url="https://t.me/fraon10k")],
+        [InlineKeyboardButton(text="✅ تحقق", callback_data="check_sub")]
+    ])
+
+
 @router.message(CommandStart())
 async def start_cmd(message):
+
+    user_name = message.from_user.first_name
 
     ok = await check_subscriptions(message.bot, message.from_user.id)
 
     if not ok:
-
-        kb = InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    InlineKeyboardButton(
-                        text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❶",
-                        url="https://t.me/feraon_1"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❷",
-                        url="https://t.me/my_botg1"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="𝗰𝗵𝗮𝗻𝗻𝗲𝗹❸",
-                        url="https://t.me/fraon10k"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="✅ تحقق",
-                        callback_data="check_sub"
-                    )
-                ]
-            ]
-        )
-
         await message.answer_photo(
             photo=START_IMAGE,
-            caption="لازم تشترك في قنوات المطور أولاً 🛡️",
-            reply_markup=kb
+            caption=f"⚠️ لازم تشترك أولاً يا {user_name} 🛡️",
+            reply_markup=subscribe_keyboard()
         )
         return
 
     await message.answer_photo(
         photo=START_IMAGE,
-        caption="👋 أهلاً بك في البوت",
+        caption=f"👋 أهلاً بك يا {user_name}\n\nتم دخولك بنجاح 🤖",
         reply_markup=main_menu()
     )
