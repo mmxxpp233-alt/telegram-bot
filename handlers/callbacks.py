3751f6846189ac1b673a2973a47d6583b4bddc7a
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 07:01:38 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,9 +1,9 @@
-from aiogram import Router, F
 import asyncio
+from aiogram import Router, F
 
-from handlers.subscription import check_subscriptions
-from keyboards.menus import main_menu, subscribe_keyboard
 from config import START_IMAGE, BOT_NAME
+from handlers.subscription import check_subscriptions
+from keyboards.menus import main_menu
 
 router = Router()
 
@@ -11,7 +11,7 @@ router = Router()
 @router.callback_query(F.data == "check_sub")
 async def check_sub(call):
 
-    # ⏳ رسالة تحميل وهمي
+    # ⏳ رسالة تحميل
     msg = await call.message.answer("⏳ جاري التحقق من الاشتراك...")
 
     await asyncio.sleep(1)
@@ -19,13 +19,13 @@ async def check_sub(call):
 
     ok = await check_subscriptions(call.bot, call.from_user.id)
 
-    # ❌ غير مشترك
+    # ❌ مش مشترك
     if not ok:
         await msg.edit_text("❌ أنت لسه مش مشترك في كل القنوات")
         await call.answer("غير مكتمل", show_alert=True)
         return
 
-    # 🟢 مشترك فعلاً
+    # 🟢 مشترك
     await msg.delete()
 
     await call.answer("✅ تم التحقق بنجاح")
@@ -41,7 +41,7 @@ async def check_sub(call):
         caption=f"""
 👋 أهلاً بك عزيزي {user_name}
 
-🤖 تم دخولك إلى القائمة الرئيسية
+🤖 تم دخولك للقائمة الرئيسية
 اختر من الأزرار 👇
 """,
         reply_markup=main_menu()
