c63076c741856ffb89b9e69f6abdd26d7801ad51
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 09:35:35 +0300

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -7,40 +7,39 @@ from keyboards.menus import main_menu
 
 router = Router()
 
+
 @router.callback_query(F.data == "check_sub")
 async def check_sub(call):
 
-msg = await call.message.answer("⏳ جاري التحقق من الاشتراك...")  
+    msg = await call.message.answer("⏳ جاري التحقق من الاشتراك...")
 
-await asyncio.sleep(1)  
-await msg.edit_text("🔍 يتم فحص القنوات...")  
+    await asyncio.sleep(1)
+    await msg.edit_text("🔍 يتم فحص القنوات...")
 
-ok = await check_subscriptions(call.bot, call.from_user.id)  
+    ok = await check_subscriptions(call.bot, call.from_user.id)
 
-if not ok:  
-    await msg.edit_text("انت لست مشترك في قنوات المطور ")  
-    await call.answer("غير مكتمل", show_alert=True)  
-    return  
+    if not ok:
+        await msg.edit_text("❌ أنت لسه مش مشترك في كل القنوات")
+        await call.answer("غير مكتمل", show_alert=True)
+        return
 
-await msg.delete()  
+    await msg.delete()
 
-await call.answer("✅ تم التحقق بنجاح")  
+    await call.answer("✅ تم التحقق بنجاح")
 
-user_name = call.from_user.first_name  
+    user_name = call.from_user.first_name
 
-await call.message.answer(  
-    f"🎉 شكراً لاشتراكك في {BOT_NAME}"  
-)  
+    await call.message.answer(
+        f"🎉 شكراً لاشتراكك في {BOT_NAME}"
+    )
 
-await call.message.answer_photo(  
-    photo=START_IMAGE,  
-    caption=f"""
-
+    await call.message.answer_photo(
+        photo=START_IMAGE,
+        caption=f"""
 👋 أهلاً بك عزيزي {user_name}
 
 🤖 تم دخولك للقائمة الرئيسية
 اختر من الأزرار 👇
 """,
-reply_markup=main_menu()
-)
-ضيف المميزات الجديده بدون متبوظ شي وهات الكود كلو استبدل القديم بي
+        reply_markup=main_menu()
+    )
