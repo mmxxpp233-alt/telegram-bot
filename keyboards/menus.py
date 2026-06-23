c33c475c41b408bcd7c197e395dabbf665c1d72c
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 12:27:36 +0300

    Update menus.py

--- keyboards/menus.py
+++ keyboards/menus.py
@@ -3,26 +3,23 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def main_menu():
     return InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="📷 قراءة QR", callback_data="qr_read"),
-            InlineKeyboardButton(text="🧾 إنشاء QR", callback_data="qr_create")
+            InlineKeyboardButton(text="📷 QR إنشاء", callback_data="qr_create"),
+            InlineKeyboardButton(text="📷 QR قراءة", callback_data="qr_read")
         ],
         [
-            InlineKeyboardButton(text="🔗 فحص الروابط", callback_data="check_link"),
-            InlineKeyboardButton(text="✂️ اختصار الروابط", callback_data="short_link")
+            InlineKeyboardButton(text="🔗 فحص رابط", callback_data="check_link"),
+            InlineKeyboardButton(text="✂️ اختصار رابط", callback_data="short_link")
         ],
         [
-            InlineKeyboardButton(text="✨ زخرفة أسماء", callback_data="decorate"),
-            InlineKeyboardButton(text="🔊 تحويل نص لصوت", callback_data="tts")
+            InlineKeyboardButton(text="🔊 تحويل صوت", callback_data="tts"),
+            InlineKeyboardButton(text="✨ زخرفة", callback_data="decorate")
         ],
         [
-            InlineKeyboardButton(text="👤 إنشاء يوزر", callback_data="new_user"),
-            InlineKeyboardButton(text="🤖 إنشاء بوت", callback_data="create_bot")
+            InlineKeyboardButton(text="👤 معلوماتك", callback_data="user_info"),
+            InlineKeyboardButton(text="🛡️ حماية IP", callback_data="ip_protect")
         ],
         [
-            InlineKeyboardButton(text="🛡️ حماية IP", callback_data="ip_protect"),
-            InlineKeyboardButton(text="ℹ️ معلوماتك كـ مستخدم", callback_data="user_info")
-        ],
-        [
+            InlineKeyboardButton(text="🔙 رجوع", callback_data="back"),
             InlineKeyboardButton(text="👨‍💻 المطور", callback_data="developer")
         ]
     ])
