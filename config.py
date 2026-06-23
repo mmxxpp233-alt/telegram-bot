5d0ab14d74d8d27bf7510ac748303b02a44e70d9
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 06:13:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-START_IMAGE = os.getenv("START_IMAGE")
+
+START_IMAGE = os.getenv("START_IMAGE")  # الرئيسية
+SUB_IMAGE = os.getenv("SUB_IMAGE")      # الاشتراك الإجباري
 
 CHANNELS = [
     "@feraon_1",
