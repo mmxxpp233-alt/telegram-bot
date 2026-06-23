3acf31d96548593bb4878b0299f96f22603e9eb7
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-23 07:11:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 
-from handlers import start, callbacks, messages
 from config import BOT_TOKEN
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(storage=MemoryStorage())
+# 📌 استيراد الروترات (الملفات الأساسية)
+from handlers import start, callbacks, messages
 
-# ربط كل الهاندلرز
-dp.include_router(start.router)
-dp.include_router(callbacks.router)
-dp.include_router(messages.router)
 
 async def main():
-    print("Bot is running...")
+
+    # 🤖 إنشاء البوت
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    # 📌 ربط الملفات (Routers)
+    dp.include_router(start.router)
+    dp.include_router(callbacks.router)
+    dp.include_router(messages.router)
+
+    # 🚀 تشغيل البوت
+    print("🚀 Bot is running...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
