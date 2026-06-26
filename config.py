070d6d593e8b5426f6cd3b6b89263665a264baa3
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-26 07:26:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,48 +1,64 @@
 import os
 
 # =========================
-# BOT
+# Telegram
 # =========================
-
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-BOT_NAME = "Feraon Tools"
+BOT_NAME = "⚡ Feraon Tools"
 
 # =========================
-# IMAGES
+# Images
 # =========================
+START_IMAGE = os.getenv(
+    "START_IMAGE",
+    "https://i.postimg.cc/Bv9zrzNb/file-00000000775071f4975454f37bb3e3ef.png"
+)
 
-START_IMAGE = os.getenv("START_IMAGE", "")
-SUB_IMAGE = os.getenv("SUB_IMAGE", "")
-
-# =========================
-# DEVELOPER
-# =========================
-
-DEVELOPER_USERNAME = "ATTACK_VlP_12"
-
-# =========================
-# BOT MAKER
-# =========================
-
-BOT_MAKER_USERNAME = "Maker_VlP_bot"
+SUB_IMAGE = os.getenv(
+    "SUB_IMAGE",
+    "https://i.postimg.cc/Bv9zrzNb/file-00000000775071f4975454f37bb3e3ef.png"
+)
 
 # =========================
-# FAKE NUMBERS WEBSITE
+# Channels
 # =========================
-
-NUMBERS_URL = "https://example.com"
-
-# =========================
-# CHANNELS
-# =========================
-
-CHANNEL_1 = "@feraon_1"
-CHANNEL_2 = "@my_botg1"
-CHANNEL_3 = "@fraon10k"
-
-REQUIRED_CHANNELS = [
-    CHANNEL_1,
-    CHANNEL_2,
-    CHANNEL_3
+CHANNELS = [
+    "@feraon_1",
+    "@my_botg1",
+    "@fraon10k",
 ]
+
+# =========================
+# Developer
+# =========================
+DEVELOPER_USERNAME = "@ALMNHRF_VIP"
+
+# =========================
+# Bot Maker
+# =========================
+BOT_MAKER_USERNAME = "@feaon_bot"
+
+# =========================
+# Fake Numbers Website
+# =========================
+FAKE_NUMBERS_URL = "https://waedo.com/"
+
+# =========================
+# Voice
+# =========================
+MALE_VOICE = "en-US-GuyNeural"
+FEMALE_VOICE = "en-US-JennyNeural"
+
+# =========================
+# QR
+# =========================
+QR_SIZE = 10
+QR_BORDER = 4
+
+# =========================
+# Username Generator
+# =========================
+USERNAME_COUNT = 10
+USERNAME_MIN = 4
+USERNAME_MAX = 6
