e39729236f18c9d87d2a430414b0f89b37021eeb
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-29 21:59:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
+
+# Handlers
+from handlers.start import router as start_router
+from handlers.subscription import router as subscription_router
