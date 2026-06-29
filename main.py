2a29a65540797f70905661010d197d16e8513820
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-29 22:32:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,21 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 
-# Handlers
 from handlers.start import router as start_router
 from handlers.subscription import router as subscription_router
 
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(BOT_TOKEN)
+
     dp = Dispatcher()
 
     dp.include_router(start_router)
     dp.include_router(subscription_router)
+
+    print("✅ Bot Started")
+
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
