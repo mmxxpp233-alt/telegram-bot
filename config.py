21e8f2dbf9a0520cf4060475a81fd044a73b72dd
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-29 22:29:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,41 +1,64 @@
 import os
 
-# ==========================
-# Telegram Bot
-# ==========================
+# =========================
+# Telegram
+# =========================
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-BOT_TOKEN = ""
+BOT_NAME = "⚡ Feraon Tools"
 
-BOT_NAME = "Feraon Tools"
-
-# ==========================
-# Developer
-# ==========================
-
-DEVELOPER_USERNAME = "YourUsername"
-
-BOT_MAKER_USERNAME = "YourUsername"
-
-# ==========================
+# =========================
 # Images
-# ==========================
+# =========================
+START_IMAGE = os.getenv(
+    "START_IMAGE",
+    "https://i.postimg.cc/Bv9zrzNb/file-00000000775071f4975454f37bb3e3ef.png"
+)
 
-START_IMAGE = ""
+SUB_IMAGE = os.getenv(
+    "SUB_IMAGE",
+    "https://i.postimg.cc/Bv9zrzNb/file-00000000775071f4975454f37bb3e3ef.png"
+)
 
-SUB_IMAGE = ""
-
-# ==========================
-# Forced Subscription
-# ==========================
-
+# =========================
+# Channels
+# =========================
 CHANNELS = [
-    "@channel1",
-    "@channel2",
-    "@channel3"
+    "@feraon_1",
+    "@my_botg1",
+    "@fraon10k",
 ]
 
-# ==========================
-# External Links
-# ==========================
+# =========================
+# Developer
+# =========================
+DEVELOPER_USERNAME = "@ALMNHRF_VIP"
 
-FAKE_NUMBERS_URL = "https://example.com"
+# =========================
+# Bot Maker
+# =========================
+BOT_MAKER_USERNAME = "@feaon_bot"
+
+# =========================
+# Fake Numbers Website
+# =========================
+FAKE_NUMBERS_URL = "https://waedo.com/"
+
+# =========================
+# Voice
+# =========================
+MALE_VOICE = "male"
+FEMALE_VOICE = "female"
+
+# =========================
+# QR
+# =========================
+QR_SIZE = 10
+QR_BORDER = 4
+
+# =========================
+# Username Generator
+# =========================
+USERNAME_COUNT = 10
+USERNAME_MIN = 4
+USERNAME_MAX = 6
