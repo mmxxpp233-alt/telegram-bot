b5492296d7231e844b885cedf88cb00bbfc295e5
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-30 10:08:59 +0300

    Update service.py

--- modules/links/service.py
+++ modules/links/service.py
@@ -4,15 +4,23 @@ import requests
 def shorten_url(url: str):
     try:
         response = requests.get(
-            "https://tinyurl.com/api-create.php",
-            params={"url": url},
+            "https://is.gd/create.php",
+            params={
+                "format": "simple",
+                "url": url
+            },
             timeout=10,
         )
 
         if response.status_code != 200:
             return None
 
-        return response.text
+        result = response.text.strip()
+
+        if result.startswith("Error:"):
+            return None
+
+        return result
 
     except Exception:
         return None
