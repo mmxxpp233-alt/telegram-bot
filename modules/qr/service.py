bca09be10fb52a816f8d7a35b24972ecbead6959
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-30 04:43:36 +0300

    Update service.py

--- modules/qr/service.py
+++ modules/qr/service.py
@@ -1,1 +1,22 @@
+import io
 
+import qrcode
+
+
+def generate_qr(text: str):
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    image = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    return buffer
