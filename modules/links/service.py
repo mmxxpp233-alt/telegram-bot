f9cbd287e49a289245665befbfcdf1265997833f
mmxxpp233-alt <mmxxpp233@gmail.com>
2026-06-30 10:03:28 +0300

    Update service.py

--- modules/links/service.py
+++ modules/links/service.py
@@ -1,1 +1,18 @@
+import requests
 
+
+def shorten_url(url: str):
+    try:
+        response = requests.get(
+            "https://tinyurl.com/api-create.php",
+            params={"url": url},
+            timeout=10,
+        )
+
+        if response.status_code != 200:
+            return None
+
+        return response.text
+
+    except Exception:
+        return None
